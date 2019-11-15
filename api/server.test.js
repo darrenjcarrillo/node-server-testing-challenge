0272c07b20bfd99125cacc98c0fbe3cfea1a839f
Darren J Carrillo <darrenjcarrillo@gmail.com>
2019-11-14 17:10:05 -0800

    DELETE WORKING

--- api/server.test.js
+++ api/server.test.js
@@ -1,6 +1,7 @@
 const request = require("supertest");
 
 const server = require("./server.js");
+const db = require("../data/dbConfig.js");
 
 it("should set db environment to testing", function() {
   expect(process.env.DB_ENV).toBe("testing");
@@ -20,26 +21,30 @@ describe("server", function() {
 
 describe("server", function() {
   describe("POST /users/", function() {
+    beforeEach(async () => {
+      await db("users").truncate();
+    });
     it("should return 200 OK", function() {
       return request(server)
         .post("/api/users")
         .send({ name: "HELLO" })
         .then(res => {
-          expect(res.status).toBe(300);
+          expect(res.status).toBe(200);
+          expect(res.body.name).toEqual("HELLO");
         });
     });
   });
 });
 
-// describe("server", function() {
-//   describe("DELETE /", function() {
-//     it("should return 200 OK", function() {
-//       return request(server)
-//         .post("/api/users")
-//         .send({ name: "HELLO" })
-//         .then(res => {
-//           expect(res.status).toBe(200);
-//         });
-//     });
-//   });
-// });
+describe("server", function() {
+  describe("DELETE /", function() {
+    it("should return 200 OK", function() {
+      return request(server)
+        .delete("/api/users/1")
+        .then(res => {
+          expect(res.status).toBe(200);
+          expect(res.body).toEqual({ message: "deleted" });
+        });
+    });
+  });
+});
--- users/userRouter.js
+++ users/userRouter.js
@@ -25,4 +25,19 @@ router.post("/users", (req, res) => {
     });
 });
 
+router.delete("/users/:id", (req, res) => {
+  const { id } = req.params;
+  Users.remove(id)
+    .then(deleted => {
+      if (deleted) {
+        res.status(200).json({ message: "deleted" });
+      } else {
+        res.status(404).json({ message: "Could not find user with given id" });
+      }
+    })
+    .catch(err => {
+      res.status(500).json({ message: "Failed to delete user" });
+    });
+});
+
 module.exports = router;
